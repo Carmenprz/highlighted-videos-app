a499965799fb781cbe7cd07d2feefc229c4786da
Carmen Pérez <cperez@thehotelsnetwork.com>
2021-11-07 20:44:55 +0100

    header and footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => (
+    <footer>
+        <p>
+            created by: Carmen
+        </p>
+    </footer>
+)
+
+export default Footer; 
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+    <div className="header-content">
+        <div className="header-title-text">Videos</div>
+        <input type="button" value="Add video" className="header-button-add"/>
+    </div>
+)
+
+export default Header; 
